ae75a47e67072bfaec6d3584cb7c9f1dd639f487
Jip1912 <jip1912@hotmail.com>
2021-08-01 14:22:33 +0200

    added exports

--- lib/flutter_chat_types.dart
+++ lib/flutter_chat_types.dart
@@ -1,17 +1,19 @@
 library flutter_chat_types;
 
+export 'src/bijlesgever.dart';
+export 'src/bijleszoeker.dart';
 export 'src/message.dart';
 export 'src/messages/custom_message.dart';
 export 'src/messages/file_message.dart';
 export 'src/messages/image_message.dart';
 export 'src/messages/partial_file.dart';
 export 'src/messages/partial_image.dart';
-export 'src/messages/partial_text.dart';
-export 'src/messages/text_message.dart';
-export 'src/messages/payment_request_message.dart';
 export 'src/messages/partial_payment_request.dart';
+export 'src/messages/partial_text.dart';
+export 'src/messages/payment_request_message.dart';
+export 'src/messages/text_message.dart';
 export 'src/messages/unsupported_message.dart';
 export 'src/preview_data.dart';
 export 'src/room.dart';
-export 'src/user_old.dart';
+export 'src/user.dart';
 export 'src/util.dart';
